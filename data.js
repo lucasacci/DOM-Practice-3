0350fc345d98c110555800448f0b74bfede10c83
Luciano <lucasacci@hotmail.com>
2022-08-22 09:16:30 -0300

    fin

--- data.js
+++ data.js
@@ -5,47 +5,46 @@ let formulario = document.querySelector("#seccion");
 
 
 let n = [];
+let i = 0;
 
-function crearTarea(){
+function crearTarea(e){
+  e.preventDefault();
+
+
     let texto = document.getElementById('tarea');   
     let card = document.createElement('div');
+    let x = i++;
     card.className = 'card my-3 mx-3 col-12 col-lg-3 col-md-4 justify-content-center'
   
 
 
     card.innerHTML = `
-    
     <div class="card-body" >
-      <h5 class="card-title">${texto.value}</h5>
+      <h5 class="card-title" id="${x}">${texto.value}</h5>
       <h6 class="card-subtitle mb-2 text-muted"></h6>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="#" class="card-link">Card link</a>
       <a href="#" class="card-link">Another link</a>
       <a href="x" class="card-link">Another link</a>
       <a href="#" class="card-link">Another link</a>
-      <button  id="#" class="btn btn-primary" onclick="eliminar(${texto.value})">Eliminar</button>
+      <button  id="#" class="btn btn-primary" onclick="eliminar(${x})">Eliminar</button>
     </div>
   `
 
 
-    n.push(texto.value);
+    n.push(x);
 
     formulario.appendChild(card);
-    console.log(formulario)
-    console.log(formulario.children)
-
-    
 
   }
   
-  function eliminar(texto){
-    
-    
+  function eliminar(x){
+     
  let pos ;
 
     for (let i = 0; i < n.length; i++) {
       
-      if(texto == n[i]){
+      if(x  == n[i]){
         pos = i;
         break;
       }
--- helpers.js
+++
@@ -1,9 +0,0 @@
-export function validarTarea(input){
-
-    if(input.value.lenght > 3 && input.value.lenght <30){
-        input.className = 'form-control is-valid'
-    }else{
-        input.className = 'form-control is-invalid'
-    }
-
-}
